a9ad64628721f55712acd6739b33c348fb3c333a
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-11-03 20:22:03 -0700

    Bug fix: Will take pizza size input from user

--- js/scripts.js
+++ js/scripts.js
@@ -30,19 +30,17 @@ $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
 
-    var userSizeSelection = $("input:radio[name=userSizeSelection]:checked").val();
+    ///better to do this on back end????
     var toppings = [];
+    /////
 
     $("input:checkbox[name=toppings]:checked").each(function(){
       var userToppingsSelections = $(this).val();
-      console.log(userToppingsSelections);
-
-      toppings.push(userToppingsSelections)
+      toppings.push(userToppingsSelections);
     });
-    console.log(toppings);
 
+    var userSizeSelection = $("input:radio[name=userSizeSelection]:checked").val();
     var pizzaOrder = new Pizza(toppings, userSizeSelection);
-    console.log(pizzaOrder);
     var pizzaOrderPrice = pizzaOrder.price();
     console.log(pizzaOrderPrice);
 
